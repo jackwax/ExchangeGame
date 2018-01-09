32358de7decaa0484356889ff1b672ee2b1e1309
zplett <zplett@oberlin.edu>
2018-01-09 15:33:48 -0500

    adding animation for head bob

--- Assets/JohnScript.cs
+++ Assets/JohnScript.cs
@@ -8,7 +8,11 @@ public class JohnScript : CharacterBehavior {
 	public float mouseSensitivity = 4f;
 	float mouseY;
 
-
+	// Used for handling the head bobbing animation 
+	public Animation movement; 
+	public CharacterController charController;
+	public bool left;
+	public bool right;
 
 
 	/* CONTROLLING */
@@ -22,6 +26,9 @@ public class JohnScript : CharacterBehavior {
 	void Start () {
 		CharacterInitialization ();
 		InitColor ();
+		// The head bobbing will work according to which foot is currently moving, starting with the left foot. 
+		left = true;
+		right = false;
 	}
 
 	void InitColor(){
@@ -47,30 +54,19 @@ public class JohnScript : CharacterBehavior {
 	public bool IsStandingOn( string tagName ) {
 		Ray groundCheck = new Ray( rb.transform.position, -Vector3.up );
 		RaycastHit groundHit;
-
 		return Physics.Raycast( groundCheck, out groundHit, distToGround + 0.1f ) &&
 		groundHit.collider.tag.Equals( tagName );
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-
 		float horizontalMove = Input.GetAxisRaw ("Horizontal");
 		float verticalMove = Input.GetAxisRaw ("Vertical");
 		movedir = (transform.right * horizontalMove + transform.forward * verticalMove).normalized;
-
-
-		Look ();
-
-		
+		Look ();	
 	}
 
 	public void Look(){
-
-
-
-
 		// Mouse look control:
 		transform.Rotate( 0f, Input.GetAxis( "Mouse X" ) * mouseSensitivity, 0f );
 		mouseY += Input.GetAxis( "Mouse Y" ) * mouseSensitivity;
@@ -78,27 +74,26 @@ public class JohnScript : CharacterBehavior {
 		Camera.main.transform.localEulerAngles = new Vector3( -mouseY, 0f, 0f ); //negative mouseY to invert
 
 		//add logic and behavior for walljumping
-
-
-
-
-
 	}
-
-
 
 	public void Move(){
 		Vector3 yVelFix = new Vector3( 0, rb.velocity.y, 0 );
 		rb.velocity = movedir * moveSpeed * Time.unscaledDeltaTime;
 		rb.velocity += yVelFix;	//allows player to be affected by gravity
+
+		// Functionality for head bobbing -- only displayed when John is moving
+		if( charController.isGrounded == true ) {
+			if (left == true) {
+				movement.Play( "left" );
+			} else if ( right == true ) {
+				movement.Play( "right" );
+			}
+		}
 	}
 
 
 	void FixedUpdate(){
 		Move ();
 	}
-
-
-
 
 }
