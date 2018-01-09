537278bbf8a8e6ab1fb2bc35944151d28e8a9e49
Helen He <helen@vpn-234-51.vpn.oberlin.edu>
2018-01-09 15:53:02 +0800

    added jackie; added navMeshAgent on jackie so that she would follow john when you press F and stop following john when you press G. Very glitchy looking.

---
+++ Assets/FollowTarget.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowTarget : MonoBehaviour {
+	public GameObject target;
+	public Rigidbody rb;
+	private NavMeshAgent agent;
+	private bool following;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		agent = GetComponent<NavMeshAgent>();
+		following = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.F)){
+			following = true;
+		}
+
+		if (Input.GetKeyDown(KeyCode.G)){
+			following = false;
+		}
+
+		if (following) {
+			follow ();
+		}
+	}
+
+	void follow () {
+		Transform targetCoor = target.GetComponent<Transform> ();
+		agent.destination = targetCoor.position;
+	}
+}
