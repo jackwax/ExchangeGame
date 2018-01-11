031a72b548076be3c41c0076751de4274a0d7fec
Unknown <jcapuder@oberlin.edu>
2018-01-11 13:27:36 -0500

    ColorManager Restructure
    Also fixed color formatting in johnscript

---
+++ Assets/ColorManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour {
+	public GameObject target;
+	public CharacterBehavior behave;
+
+	//startColor: Initial color value of the target's material.
+	public Color lastColor;
+
+	//endColor: The final color value for target. Initially the same as startColor.
+	public Color endColor;
+
+	//rend: Renderer component of target game object.
+	public Renderer rend;
+
+	//toLerp: boolean flag for use in update(). Set to true when new endColor is set.
+	public bool toLerp = false;
+
+	//Params for mechanics of color interpolation. 
+	public float lerpDuration = 2.0f;
+	public float t=0.0f;
+
+
+	void Start () {
+		target = behave.gameObject;
+		rend = target.GetComponent<Renderer> ();
+		lastColor = behave.colorlist[behave.mood];
+	}
+
+	/*
+	 * Set the target color to interpolate to.
+	 * The actual process of changing the color happens in update(), so we also set the boolean flag toLerp to true.
+	 */
+	void ColorMoodChange(int newMood){
+		endColor = behave.colorlist[newMood];
+		toLerp = true;
+	}
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (toLerp) {
+			if (t <= 1) {
+				Color c = Color.Lerp (lastColor, endColor, t);
+				t += Time.deltaTime / lerpDuration;
+				rend.material.color = c;
+			}else{
+				toLerp = false;
+				lastColor = endColor;
+				t = 0.0f;
+			}
+		}
+
+	}
+}
